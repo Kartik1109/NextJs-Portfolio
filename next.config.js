b23fe02cabe2bed4dfce792542e8e6b7ac481f87
Kartik_1109 <kartutanu@gmail.com>
2024-03-29 13:14:14 -0400

    revert

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
-  output: "export",
+  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "js"],
   eslint: {
